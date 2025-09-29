d321eceaf8aa85b64a54681255a3d24da8319cf7
DanHuy142005 <nhangu255@gmail.com>
2025-09-29 21:59:10 +0700

    Update Board.java

--- xiangqi/src/xiangqi/core/Board.java
+++ xiangqi/src/xiangqi/core/Board.java
@@ -34,7 +34,40 @@ public class Board {
     public void setPieces(int rank, int file, int piece) {
     	squares[index(rank, file)] = piece;
     }
+    public static Board initial() {
+    	Board b = new Board();
+    	int row0[] = {Pieces.bR, Pieces.bN, Pieces.bE, Pieces.bA, Pieces.bK,
+                Pieces.bA, Pieces.bE, Pieces.bN, Pieces.bR }; 
+    	for (int j = 0; j < files; j++) {
+    		b.setPieces(0, j, row0[j]);
+    	}
+    	b.setPieces(2, 1, Pieces.bC);
+    	b.setPieces(2, 7, Pieces.bC);
+    	for (int j : new int[]{0,2,4,6,8}) b.setPieces(3, j, Pieces.bP);
+    	for (int c : new int[]{0,2,4,6,8}) b.setPieces(6, c, Pieces.rP);
+    	b.setPieces(7, 1, Pieces.rC);
+    	b.setPieces(7, 7, Pieces.rC);
+    	int[] row9 = { Pieces.rR, Pieces.rN, Pieces.rE, Pieces.rA, Pieces.rK,
+                Pieces.rA, Pieces.rE, Pieces.rN, Pieces.rR };
+        for (int c = 0; c < files; c++) b.setPieces(9, c, row9[c]);
+        return b;
+    }
+    public void print() {
+    	 StringBuilder sb = new StringBuilder();
+         for (int r = 0; r < ranks; r++) {
+             for (int f = 0; f < files; f++) {
+                 int piece = getPieces(r, f);
+                 sb.append(Pieces.symbol(piece))       
+                   .append(' ');
+             }
+             sb.append('\n');
+         }
+         System.out.print(sb.toString());
+         System.out.println("Side to move: " + (sideToMove == red ? "RED" : "BLACK"));	
+    }
 	public static void main(String[] args) {
-		
+		Board b = Board.initial();
+        b.print();
 	}
 }
+
